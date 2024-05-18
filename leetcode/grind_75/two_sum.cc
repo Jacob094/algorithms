8ff89e5409f825a6df0dc527d39a4b81f479c94a
Jacob Ogle <jacbcodes@gmail.com>
2024-05-17 22:00:10 -0400

    restructure and added start of grind75

---
+++ leetcode/grind_75/two_sum.cc
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/two-sum/description/
+
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+   
+    vector<int> twoSumBruteForce(vector<int>& nums, int target) {
+            
+    }
+   
+   
+    vector<int> twoSumOptimzed(vector<int>& nums, int target) {
+            
+    }
+};
